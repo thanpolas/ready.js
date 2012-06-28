fbd2aa9003b63b3f75324daeea57d01c85fb6aff
Thanasis Polychronakis <thanpolas@gmail.com>
2012-06-28 22:07:09 +0300

    All tests pass

--- test/unit/ready.test.js
+++ test/unit/ready.test.js
@@ -28,9 +28,8 @@ var getParams = function(opt_JSON)
   }
 };
 
-
 test('Core functionality', function() {
-  expect( 4 );
+  expect( 8 );
 
   stop();
 
@@ -41,8 +40,19 @@ test('Core functionality', function() {
   function hookTwo()
   {
     ok(true, 'Listener Two executed');
-    start();
   }
+  
+  function hookThree()
+  {
+    ok(true, 'Listener Three executed');
+  }
+  function hookFour()
+  {
+    ok(true, 'Listener Four executed');
+    start();    
+  }  
+
+  ss.ready(hookOne);
 
   var main = ss.ready('main');
 
@@ -51,16 +61,23 @@ test('Core functionality', function() {
   main.addCheck('task2');
 
   // add our listeners
-  main.addListener(hookOne);
   main.addListener(hookTwo);
-
+  main.addListener(hookThree);
+  
   ok(!main.isDone(), 'Ready watch is not done yet');
 
   // complete the checks
+  ok(!main.isDoneCheck('task1'), 'task 1 is not done yet');
   main.check('task1');
-  main.check('task2');
+  ok(main.isDoneCheck('task1'), 'task 1 is now done');
+  
+  // get main watch in another variable
+  var niam = ss.ready('main');
+  niam.addListener(hookFour);
+  
+  niam.check('task2');
 
-  ok(main.isDone(), 'Ready watch is now done');
+  ok(niam.isDone(), 'Ready watch is now done');
 });
 
 
@@ -80,40 +97,164 @@ test('Sequence of execution', function() {
       }
     };
   }
-  
+
   var r = ss.ready('a-ready-watch');
-  
+
   // add checks
   r.addCheck('task1');
   r.addCheck('task2');
   r.addCheck('task3');
-  
+
   // add listeners
   r.addListener(addHook(7, 'First main hook executed'));
-  r.addListener(addHook(8, 'Second main hook executed'));  
-  r.addListener(addHook(9, 'Lazy hook executed'), 300);
-  r.addListener(addHook(10, 'Very lazy hook executed', true), 600);  
+  r.addListener(addHook(8, 'Second main hook executed'));
+  r.addListener(addHook(9, 'Lazy hook executed'), 100);
+  r.addListener(addHook(10, 'Very lazy hook executed', true), 150);
   r.addCheckListener('task1', addHook(1, 'Task1 check hook executed'));
   r.addCheckListener('task2', addHook(3, 'Task2 check hook executed'));
   r.addCheckListener('task2', addHook(5, 'Task2 check lazy hook executed'), 50);
   r.addCheckListener('task3', addHook(6, 'Task3 check hook executed'));
-      
+
   ok(!r.isDoneCheck('task1'), 'task1 is not done yet');
 
   // complete the checks
   r.check('task1');
-  ok(r.isDoneCheck('task1'), 'task1 is now done'); 
-  
+  ok(r.isDoneCheck('task1'), 'task1 is now done');
+
   equal(seq, 2, 'After task1 is done');
-  seq++;  
-  
+  seq++;
+
   r.check('task2');
 
   equal(seq, 4, 'After task2 is done');
-  seq++;  
-  
+  seq++;
+
   // execute after 100ms
   setTimeout(function(){
-    r.check('task3');    
-  }, 100);
+    r.check('task3');
+  }, 80);
+});
+
+
+test('Canceling listeners', function() {
+  expect( 4 );
+
+  stop();
+
+  var seq = 1;
+
+  function addHook(testSeq, title, opt_done)
+  {
+    return function(){
+      equal(seq, testSeq, title);
+      seq++;
+      if (opt_done) {
+        start();
+      }
+    };
+  }
+
+  function addFalseHook(title)
+  {
+    return function(){
+      ok(false, title);
+      start();
+    };
+
+  }
+
+  var r = ss.ready('a-ready-watch-rumble');
+
+  // add checks
+  r.addCheck('task1');
+  r.addCheck('task2');
+  r.addCheck('task3');
+
+  // add listeners
+  r.addListener(addHook(1, 'First main hook executed'));
+  r.addListener(addHook(2, 'Second main hook executed'));
+  r.addListener(addHook(3, 'Lazy hook executed'), 30);
+  r.addListener(addHook(4, 'Very lazy hook executed', true), 60);
+  var removeOne = r.addListener(addFalseHook('First removed hook'));
+  var removeTwo = r.addListener(addFalseHook('Second removed hook'), 10);
+  var removeThree = r.addCheckListener('task1', addFalseHook('Third removed check hook'));
+  var removeFour = r.addCheckListener('task1', addFalseHook('Fourth removed check hook'), 20);
+
+  r.check('task2');
+  r.check('task3');
+  r.removeListener(removeOne);
+  r.removeListener(removeTwo);
+  r.removeCheckListener(removeThree);
+  r.removeCheckListener(removeFour);
+  r.check('task1');
+});
+
+
+
+test('Multiple Ready Watches', function() {
+
+  expect( 28 );
+
+  var seq = 1;
+  function addHook(testSeq, title, opt_done)
+  {
+    return function(){
+      equal(seq, testSeq, title);
+      seq++;
+      if (opt_done) {
+        start();
+      }
+    };
+  }
+
+  // 14 names
+  var readyNames = ['apple', 'oragne', 'banana', 'alpha', 'bravo', 'charlie', 'delta',
+      'echo', 'foxtrot', 'golf', 'hotel', 'India', 'JulieT', 'Kilo'];
+
+  // create ready watches and add checks
+  var readyWatches = [];
+  var r = null;
+  for(var i = 0, l = readyNames.length; i < l ; i++) {
+    r = ss.ready(readyNames[i]);
+    r.addCheck('task1');
+    r.addCheck('task2');
+    r.addCheck('task3');
+  }
+
+  // add listeners to ready watches
+  for(i = 0 ; i < l ; i++) {
+    r = ss.ready(readyNames[i]);
+    r.addListener(addHook(i + i + 1, readyNames[i] + ' hook executed No1'));
+    r.addListener(addHook(i + i + 2, readyNames[i] + ' hook executed No2'));
+  }
+
+  // Complete checks
+  for(i = 0 ; i < l ; i++) {
+    r = ss.ready(readyNames[i]);
+    r.check('task1');
+    r.check('task2');
+    r.check('task3');
+  }
+  
+
+});
+
+test('Exceptional cases', function() {
+  
+  raises(function(){
+    var r = ss.ready();
+  }, TypeError, 'No params on ready init should raise TypeError');
+  
+  raises(function(){
+    var r = ss.ready({});
+  }, TypeError, 'Object parameter passed on ready init should raise TypeError');
+
+  raises(function(){
+    var r = ss.ready([]);
+  }, TypeError, 'Array parameter passed on ready init should raise TypeError');
+
+  raises(function(){
+    var r = ss.ready(true);
+  }, TypeError, 'Boolean parameter passed on ready init should raise TypeError');  
+  
 });
